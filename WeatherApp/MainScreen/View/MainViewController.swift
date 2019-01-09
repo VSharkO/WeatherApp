7d48df812da990f0e1e2fb20556f7a61f9b701ce
VSharkO <valentin.saric1@gmail.com>
2019-01-09 08:40:40 +0100

    reverted constraints changes

--- WeatherApp/MainScreen/View/MainViewController.swift
+++ WeatherApp/MainScreen/View/MainViewController.swift
@@ -487,7 +487,7 @@ class MainViewController: UIViewController, LoaderManager, UIViewControllerTrans
             ])
         
         NSLayoutConstraint.activate([
-            bodyImageView.topAnchor.constraint(equalTo: self.view.topAnchor),
+            bodyImageView.topAnchor.constraint(equalTo: self.view.topAnchor, constant: self.view.frame.height*0.2),
             bodyImageView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
             bodyImageView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
             bodyImageView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor)
